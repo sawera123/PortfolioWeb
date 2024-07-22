3a498c949791fac99f008e2a9bba44d13b36517f
sawera123 <saweragulshan89@gmail.com>
2024-07-22 00:47:28 -0700

    upload

--- src/Pages/JournalPage.jsx
+++ src/Pages/JournalPage.jsx
@@ -1,14 +1,14 @@
 import React from 'react'
 // import Preloader from '../components/Preloader'
 import Header from '../components/Header'
-import JournalProtection from '../components/JournalProtection'
+import Journal from "../components/Journal"
 import Footer from '../components/Footer'
 export default function JournalPage() {
   return (
     <>
      {/* <Preloader/> */}
     <Header/>
-    <JournalProtection/>
+    <Journal/>
     <Footer/>
     </>
   )
--- src/components/Store.jsx
+++ src/components/Store.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+import React ,{useEffect} from 'react'
 import { useTranslation } from "react-i18next"
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+import 'typer-js';
 
 export default function Store() {
   const { t } = useTranslation();
+
+  useEffect(() => {
+    // Initialize AOS
+    AOS.init({
+      duration: 2200,
+      delay: 1000,
+      once: true,
+    });
+
+    // Initialize typer.js for the typing effect
+    const typerElement = document.getElementById('main');
+    const typer= new window.Typer(typerElement, {
+      // strings: ['Developer'],
+      strings: [t('Coming Soon....')],
+      delay: 1000,
+      colors: ['#25262f'],
+      endless: true,
+      waitUntilVisible: true,
+    });
+  }, [t]);
+ 
   return (
     <>
         <div className="container-fluid d-flex align-items-center justify-content-center min-vh-100 "
@@ -11,8 +35,15 @@ export default function Store() {
             <div className="row justify-content-center">
                 <div className="col-10 text-center">
 
-                    <h1 className="tect-white mt-5" style={{"color": "white"}}> {t("Coming Soon........")} </h1>
-
+                    {/* <h1 className="tect-white mt-5" style={{"color": "white"}}> {t("Coming Soon........")} </h1> */}
+                    <h1
+                          className="typer primary-color text-white mt-5 pl-2"
+                          id="main"
+                          style={{"color": "white"}}
+                          data-words={t("Coming Soon....")}
+                          data-delay="100"
+                          data-colors="#25262f"
+                        ></h1>
 
                 </div>
             </div>
