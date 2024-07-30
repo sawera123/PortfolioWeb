8e234f3007ade662e48a8f1be69799c144f79314
sawera123 <saweragulshan89@gmail.com>
2024-07-30 05:28:23 -0700

    llll

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,16 +14,16 @@ export default function Footer() {
                     <div className="row p-4">
                         <div className="col-xl-4  col-lg-5  col-md-8  col-sm-7 col-12">
                             <div className="footer-widget footer-logo pb-40">
-                                <div className="foot-logo mb-30">
+                                {/* <div className="foot-logo mb-30">
                                     <img src="images/logo/image.gif"
                                     alt="THAMES"/>
-                                </div>
+                                </div> */}
                                 <div 
       className="foot-logo mb-30"
       onMouseEnter={() => setIsHovered(true)}
       onMouseLeave={() => setIsHovered(false)}
     >
-      <Link to='/' className="header-logo">
+      <Link to='/' className="">
         <img 
           src={isHovered ? "images/logo/hover.gif" : "images/logo/image.gif"} 
          
